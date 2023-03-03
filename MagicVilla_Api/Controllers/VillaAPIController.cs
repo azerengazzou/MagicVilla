e873aa29d2043a2ca105c22290dc0dfa41a0441f
azerengazzou <engazzouazer1@gmail.com>
2023-03-03 12:26:19 +0100

    models-controllers

---
+++ MagicVilla_Api/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_Api.Data;
+using MagicVilla_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Api.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.VillaList;
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.VillaList.FirstOrDefault(u => u.Id==id);
+        }
+    }
+}
---
+++ MagicVilla_Api/Data/VillaStore.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_Api.Models;
+
+namespace MagicVilla_Api.Data
+{
+    public class VillaStore 
+    {
+        public static List<VillaDTO> VillaList = new List<VillaDTO>
+            {
+                new VillaDTO
+                {
+                    Id = 1,
+                    Name = "Pool View",
+                },
+                new VillaDTO{
+                    Id= 2,
+                    Name="Ocean View",
+
+                }
+            };
+    }
+}
